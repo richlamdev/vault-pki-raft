8ba0aaa56596635325264d2e537c6aecb4f5160f
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2024-12-24 22:39:48 -0800

    reduce variables across scripts

--- env.sh
+++ env.sh
@@ -1,23 +1,33 @@
-export VAULT_ADDR="http://127.0.0.1:8200"
-
+### issue_cert_template.sh variables
 HOST_STRING="${1:-$HOST_STRING}"
 SLD_STRING="middleearth"
 TLD_STRING="test"
 DOMAIN_STRING="${SLD_STRING}.${TLD_STRING}"
-SUBJECT_CN_STRING="${HOST_STRING}.${DOMAIN_STRING}"
+SUBJECT_CN="${HOST_STRING}.${DOMAIN_STRING}"
 ISSUER_NAME_CN_STRING="Lord of the Rings"
-
-VAULT_ROLE_STRING="common_vault_role"
-NO_TLS_STRING="-tls-skip-verify"
-KEY_TYPE_STRING="ec"
-KEY_BITS_STRING="384"
-
-IP_SAN1_STRING="192.168.60.6"
-ALT_NAME1_STRING="${SUBJECT_CN_STRING}"
-
+IP_SAN1="192.168.60.6"
+ALT_NAME1="${SUBJECT_CN}"
 #IP_SAN2_STRING=""
 #ALT_NAME2_STRING=""
-TTL_STRING="9528h"
+TTL="9528h"
+
+### create_root_inter_certs.sh variables
+DOMAIN="$DOMAIN_STRING"
+ISSUER_NAME_CN="$ISSUER_NAME_CN_STRING"
+CN_ROOT="${ISSUER_NAME_CN} Root Certificate Authority"
+CN_INTER="${ISSUER_NAME_CN} Intermediate Certificate Authority"
+CN_ROOT_NO_SPACE="${CN_ROOT// /_}"
+CN_INTER_NO_SPACE="${CN_INTER// /_}"
+ROOT_DIR="./root_certs"
+INTERMEDIATE_DIR="./intermediate_certs"
+
+### common variables
+# VAULT_ROLE_STRING="common_vault_role"
+export VAULT_ADDR="http://127.0.0.1:8200"
+VAULT_ROLE="common_vault_role"
+NO_TLS="-tls-skip-verify"
+KEY_TYPE="ec"
+KEY_BITS="384"
 
 # Define color codes
 BLACK='\033[0;30m'
--- raft.sh
+++ raft.sh
@@ -15,14 +15,14 @@ NO_TLS="-tls-skip-verify"
 #VAULT_ADDR="http://127.0.0.1:8200"
 
 # Define colors from env.sh
-BLACK='\033[0;30m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
-WHITE='\033[0;37m'
-NC='\033[0m' # No Color
+# BLACK='\033[0;30m'
+# RED='\033[0;31m'
+# GREEN='\033[0;32m'
+# YELLOW='\033[0;33m'
+# MAGENTA='\033[0;35m'
+# CYAN='\033[0;36m'
+# WHITE='\033[0;37m'
+# NC='\033[0m' # No Color
 
 function stop_vault {
 
